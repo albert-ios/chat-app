4310ef9bcc1912ad72e8c421e4cd3ab8c039787f
albert-ios <albert231207l@mail.ru>
2026-06-21 15:56:19 +0300

    feat(client): integrate redux-saga websocket channel and store provider

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,7 @@
 import * as types from '../constants/ActionTypes';
 
 let nextMessageId = 0;
+let nextUserId = 0;
 
 export const addMessage = (message, author) => ({
   type: types.ADD_MESSAGE,
@@ -19,4 +20,10 @@ export const messageReceived = (message, author, id) => ({
 export const populateUsersList = users => ({
   type: types.USERS_LIST,
   users
+});
+
+export const addUser = name => ({
+  type: types.ADD_USER,
+  id: nextUserId++,
+  name
 });
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import chat from './reducers';
+import handleNewMessage from './sagas';
+import setupSocket from './sockets';
+import { addUser } from './actions';
+import App from './App.jsx';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  chat,
+  applyMiddleware(sagaMiddleware)
+);
+
+// Элементарная, но надежная псевдо-авторизация при входе
+const username = prompt('Введите ваше имя для чата:', 'Студент') || 'Студент';
+
+const socket = setupSocket(store.dispatch, username);
+
+sagaMiddleware.run(handleNewMessage, { socket, username });
+
+store.dispatch(addUser(username));
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
     <App />
-  </StrictMode>,
-)
+  </Provider>
+);
